b0360cc1e237a3e754ddde76d4104ef5030690a5
Vladislav Bobrovnikov <bobrovnikov.v.i@gmail.com>
2024-12-18 17:42:14 +0300

    chore: rename stack controller methods

--- src/controllers/stack.controller.schema.ts
+++ src/controllers/stack.controller.schema.ts
@@ -1,6 +1,6 @@
 import z from 'zod';
 
-export const AddItemSchema = z.object({
+export const AddStackItemSchema = z.object({
   body: z
     .object({
       item: z.union([z.string().min(1), z.number()]),
